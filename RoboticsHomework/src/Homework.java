fcf307ce85ed7a29a869a7df69e546d4d75030a3
jaackyauyeung <31105341+jaackyauyeung@users.noreply.github.com>
2017-10-03 20:50:42 -0700

    This is my first homework assignment!

---
+++ RoboticsHomework/src/Homework.java
@@ -0,0 +1,11 @@
+/*
+ * This class prints out three line of statements to the console.
+ */
+public class Homework { //(Class) The class is the a blueprint or a template for creating different objects which defines its properties and behaviors.
+public static void main(String[] args) //(Main Method) The main method is The main() is a function that calls all the other functions required to run your program. 
+{
+	System.out.println("We have a robotics meeting after school tomorrow"); //(Statement) a complete command to be executed and can include one or more expressions.
+	System.out.println("Robotics meetings proceed from 3:30-5 pm"); //(Statement) a complete command to be executed and can include one or more expressions.
+	System.out.println("Robotics meetings are a lot of fun!"); //(Statement) a complete command to be executed and can include one or more expressions.
+}
+}
