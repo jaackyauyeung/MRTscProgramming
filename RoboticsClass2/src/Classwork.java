2928f669c1f326411b69da246d0c513a2c2f6df9
jaackyauyeung <31105341+jaackyauyeung@users.noreply.github.com>
2017-10-04 16:51:39 -0700

    This is the classwork we did in class today for robotics.

---
+++ RoboticsClass2/src/Classwork.java
@@ -0,0 +1,10 @@
+/*
+ * This class will demonstrate my understanding of the 5 time data types we learned in robotics class today.
+ */
+public class Classwork {
+int x = 1;
+double y = 1.0;
+char c = 'c';
+String leonard = "tutor";
+boolean eric = false;
+}
